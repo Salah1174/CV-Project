8b0dea1b2e3d90e809f711f8296d157710dd205c
salma-h-wagdy <salmahisham139@gmail.com>
2024-11-22 16:39:54 +0200

    detects skewed , incomplete barcodes

--- ErosionAndDilation.py
+++ ErosionAndDilation.py
@@ -5,7 +5,7 @@ import numpy as np
 
 # This function reorder the corners points appropriatly 
 # Helped significantly with warp function
-def reorder(myPoints):
+def reorder(myPoints):    #salma yousef
     myPoints = myPoints.reshape((4, 2))
     myPointsNew = np.zeros((4, 1, 2), dtype=np.int32)
     add = myPoints.sum(1)
@@ -16,7 +16,37 @@ def reorder(myPoints):
     myPointsNew[2] = myPoints[np.argmax(diff)]
     return myPointsNew
 
-def detect_barcode(image):
+def make_columns_uniform(image):
+    # Get the dimensions of the image
+    height, width = image.shape
+    
+    # Iterate through each column
+    for x in range(width):
+        # Get the pixels in the current column
+        column_pixels = image[:, x]
+        cnt = [0,0]
+        # Find the most common pixel value in the column
+        unique, counts = np.unique(column_pixels, return_counts=True)
+        mode_pixel = unique[np.argmax(counts)]
+
+        for idx, item in enumerate(unique):
+            if(item >= 128):
+                unique[idx] = 255
+                cnt[0]+=1
+            else:
+                unique[idx] = 0
+                cnt[1]+=1
+
+        print(unique)
+        print(unique[np.argmax(counts)])
+        # Set all pixels in the column to the most common value
+        image[:, x] = 0 if cnt[1] > cnt[0] else 255
+    
+
+    return image
+
+
+def detect_barcode(image):   #salma yousef
     
     # 1---->canny
 
@@ -63,20 +93,15 @@ def detect_barcode(image):
     #  keeping only the largest one
     (cnts, _) = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
     cnt = sorted(cnts, key = cv2.contourArea, reverse = True)[0] 
+    
     if cv2.contourArea(cnt) > 0:  # Check if contour is valid
 
         # compute the rotated bounding box of the largest contour
         rect = cv2.minAreaRect(cnt)
         box = np.int32(cv2.boxPoints(rect))
-        
-        # box coordinates
-        x, y, w, h = cv2.boundingRect(box) 
         box = reorder(box)
-        # print(box)
-
-        # Crop the barcode 
-        cropped_barcode = image[y:y+h, x:x+w] #Salma Hisham
-
+        
+       
         # Coordinates of each corner
         ax = box.item(0)
         ay = box.item(1)
@@ -89,20 +114,52 @@ def detect_barcode(image):
 
         dx = box.item(6)
         dy = box.item(7)
+
+        # box coordinates
+        x, y, w, h = cv2.boundingRect(box) 
+        
+        # print(box)
+        border_threshold = 10
+        # height, width = image.shape[:2]
+        widthA = np.sqrt(((cx - dx) ** 2) + ((cy - dy) ** 2))
+        widthB = np.sqrt(((ax - bx) ** 2) + ((ay - by) ** 2))
+        width = max(int(widthA), int(widthB))
+        
+        heightA = np.sqrt(((ax - dx) ** 2) + ((ay - dy) ** 2))
+        heightB = np.sqrt(((bx - cx) ** 2) + ((by - cy) ** 2))
+        height = max(int(heightA), int(heightB))
+        
+        print(width, height)
+        print (x, y, w, h)
+        print(ax, ay, bx, by, cx, cy, dx, dy)
+        if x < border_threshold:
+            x = 0
+        if x + w > width - border_threshold:
+            w = width - x
+        if y < border_threshold:
+            y = 0
+        if y + h > height - border_threshold:
+            h = height - y
+
+        # Draw the extended bounding box
+        # cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+        # Crop the barcode 
+        cropped_barcode = image[y:y+height, x:x+width] #Salma Hisham
+
         
         pts1 = np.float32([[bx, by], [ax, ay], [cx, cy], [dx, dy]])
-        pts2 = np.float32([[0, 0], [w, 0], [0, h], [w, h]])
+        pts2 = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
 
         matrix = cv2.getPerspectiveTransform(pts1, pts2)
-        img_prespective = cv2.warpPerspective(image, matrix, (w, h))
+        img_prespective = cv2.warpPerspective(image, matrix, (width, height))
         # cv2.circle(image, (ax,ay), 5, (0,0,255), 20)
         # cv2.circle(image, (bx,by), 1, (0,0,255), 10)
         # cv2.circle(image, (cx,cy), 1, (0,0,255), 10)
         # cv2.circle(image, (dx,dy), 1, (0,0,255), 10)
-
+        
         # draw a bounding box arounded the detected barcode and display the image
         cv2.drawContours(image, [box], -1, (0, 255, 0), 3) 
-
 
 
         # cv2.imshow('Gradient', gradient)
@@ -110,7 +167,11 @@ def detect_barcode(image):
         cv2.imshow('closed2', closed) #Salma Hisham
         cv2.imshow("Image", image) #salma hisham
         cv2.imshow('Cropped Barcode', cropped_barcode) #Salma Hisham
-        cv2.imshow("Warp", img_prespective)
+        cv2.imshow("Warp", img_prespective) #salma yousef
+
+        # uniform_image = make_columns_uniform(img_prespective)
+        # cv2.imshow("Uniform Image", uniform_image)
+        
     cv2.waitKey(0)
 
 
@@ -149,9 +210,9 @@ img = cv2.imread(
     # "Test Cases\\06 - meen fata7 el nour 333eenaaayy.jpg", 0)
     # "Test Cases\\07 - mal7 w felfel.jpg", 0)
     # "Test Cases\\08 - compresso espresso.jpg", 0)
-    # "Test Cases\\09 - e3del el soora ya3ammm.jpg", 0)
+    "Test Cases\\09 - e3del el soora ya3ammm.jpg", 0)
     # "Test Cases\\10 - wen el kontraastttt.jpg", 0)
-    "Test Cases\\11 - bayza 5ales di bsara7a.jpg", 0)
+    # "Test Cases\\11 - bayza 5ales di bsara7a.jpg", 0)
     # "Barcode_Noise_3.jpg", 0)
 
 
@@ -217,7 +278,7 @@ cv2.imshow('Bilateral Dilation Erosion Erosion', bilateral_filt_dil_ero_ero)
 # cv2.imshow('Laplacian Ero', img_dst_ero)
 
 # cv2.imshow('Median', img_dst_median)
-detect_barcode(bilateral_filt_dil_ero_ero)
+detect_barcode(bilateral_filt_dil_ero)
 
 
 cv2.waitKey(0)
